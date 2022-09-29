e9f807625621c2c29053183031d32267e72028d4
Aaron Jones <ajones@c1r5p18.hive.fi>
2022-09-29 13:55:45 +0300

    added comments

--- srcs/manage_map.c
+++ srcs/manage_map.c
@@ -6,11 +6,18 @@
 /*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 14:18:14 by ajones            #+#    #+#             */
-/*   Updated: 2022/09/29 01:31:06 by ajones           ###   ########.fr       */
+/*   Updated: 2022/09/29 13:55:21 by ajones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/filler.h"
+
+/* 
+** map_value checks for the the character concerning each player, uppercase and
+** lowercase. If it see the '.' then it will have a value of 0. If the
+** character is the one that I have been assigned, it will be given the value
+** of -1. The enemies character will be given -2
+*/
 
 void	map_value(t_filler *data, int y, char *line)
 {
@@ -29,6 +36,12 @@ void	map_value(t_filler *data, int y, char *line)
 	}
 }
 
+/*
+** This will read the lines until it is at the actual grid and disregard the 
+** numbers before the grid on each line. Because it is a 2D array, it will loop
+** through each line sending the the line to the next function.
+*/
+
 void	read_map(t_filler *data)
 {
 	char	*line;
@@ -45,6 +58,10 @@ void	read_map(t_filler *data)
 		line_count++;
 	}
 }
+
+/*
+** Standard mallocing for a 2D int array using map height and width
+*/
 
 int	**make_map(t_filler *data)
 {
@@ -71,6 +88,12 @@ int	**make_map(t_filler *data)
 	}
 	return (map);
 }
+
+/* 
+** the got_map variable is basically a switch that tells us whether
+** this is the first time coming into this function or not. If it is
+** the first time. It will be the only time that the map will be malloced 
+*/
 
 int	manage_map(t_filler *data)
 {
