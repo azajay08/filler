fd8d518d438faff8abf2993b9d7f8791810099b9
Aaron Jones <aaron.p.j@hotmail.com>
2022-10-11 15:12:11 +0300

    macro name change for tester

--- resources/tests/map0_1.sh
+++ resources/tests/map0_1.sh
@@ -1,50 +1,52 @@
-#! /usr/bin/env bash
+#!/bin/bash
+
+name= ajones
 
 echo "\n\n-----*****abanlin map00*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/abanlin.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/abanlin.filler -p2 ../${name}.filler -q; done
 
 
 echo "\n\n-----*****abanlin map01*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/abanlin.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/abanlin.filler -p2 ../${name}.filler -q; done
 
 echo "\n\n-----*****carli map00*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/carli.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/carli.filler -p2 ../${name}.filler -q; done
 
 echo "\n\n-----*****carli map01*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/carli.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/carli.filler -p2 ../${name}.filler -q; done
 
 echo "\n\n-----*****champely map00*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/champely.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/champely.filler -p2 ../${name}.filler -q; done
 
 echo "\n\n-----*****champely map01*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/champely.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/champely.filler -p2 ../${name}.filler -q; done
 
 echo "\n\n-----*****grati map00*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/grati.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/grati.filler -p2 ../${name}.filler -q; done
 
 echo "\n\n-----*****grati map01*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/grati.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/grati.filler -p2 ../${name}.filler -q; done
 
 echo "\n\n-----*****hcao map00*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/hcao.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/hcao.filler -p2 ../${name}.filler -q; done
 
 echo "\n\n-----*****hcao map01*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/hcao.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/hcao.filler -p2 ../${name}.filler -q; done
 
 echo "\n\n-----*****superjeannot map00*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/superjeannot.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map00 -p1 players/superjeannot.filler -p2 ../${name}.filler -q; done
 
 echo "\n\n-----*****superjeannot map01*****-----\n\n"
 
-for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/superjeannot.filler -p2 ../ajones.filler -q; done
+for i in {1..5}; do ./filler_vm -f maps/map01 -p1 players/superjeannot.filler -p2 ../${name}.filler -q; done
